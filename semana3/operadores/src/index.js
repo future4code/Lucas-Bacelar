6427d373d8f5b475715ef3178c2572fcb27d2ade
Lucas Bacelar <lucasbacela@hotmail.com>
2021-02-23 15:20:31 -0300

    Resolução das atividades de interpretação

---
+++ semana3/operadores/src/index.js
@@ -0,0 +1,20 @@
+// INTERPRETAÇÃO --------------------------------------------------------------------------
+
+// 1ª questão ----------------------------------
+
+// a. vai imprimir false
+// b. vai imprimir false
+// c. vai imprimir true
+// d. o tipo vai ser 'boolean'
+
+// 2ª questão ----------------------------------
+
+// a. vai imprimir o tipo undefined
+// b. vai imprimir o tipo null
+// c. vai imprimir o valor 10
+// d. vai imprimir o valor 3
+// e. vai imprimir [3, 19, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+// f. vai imprimir o valor 9
+
+
+// ESCRITA ----------------------------------------------------------------------------------
