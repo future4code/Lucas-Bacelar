5a4c6e60a15e19b7afdb2366061861a225e8db80
Lucas Bacelar <lucasbacela@hotmail.com>
2021-02-24 12:22:18 -0300

    Resolução dos desafios de interpretação

---
+++ semana3/condicionais/src/index.js
@@ -0,0 +1,28 @@
+// *ENUNCIADO --------------------------------------------------------------------------------------
+
+// INTERPRETAÇÃO --------------------------------------------
+
+// Exercicio 1 ------------
+
+// a. O código checa se um numero é par ou não
+// b. Ele checa se o resto da divisão do numero por 2 é igual a 0
+// c. Para numeros em que o resto da divisão por 2 é igual a 0 ou seja numeros pares.
+// d. Para numeros impares, cujo resto da divisão não é 0.
+
+// Exercicio 2 -------------
+
+// a. O código serve para mostrar o preço da fruta escolhida pelo usuário.
+// b. R$ 2,25.
+// c. A mensagem seria "O preço da fruta pêra é R$5"
+
+// Exercicio 3 -------------
+
+// a. Está pedindo um numero ao usuário que vem no tipo "string" e está convertendo ele pra o tipo "number".
+// b. Se o numero fosse 10 ele imprimiria "Esse número passou no teste", se não se fosse -10 ele não imprimiria mensagem nenhuma.
+// c. Sim hávera, pois o escopo pai está chamando a variável "mensagem" que só existe no escopo filho, fazendo com que ocorra 
+// um erro de variável não declarada.
+
+
+
+
+
