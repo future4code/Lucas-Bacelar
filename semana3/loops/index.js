9c204640e0a47881a8f644af0c3f60b1a9db6653
Lucas Bacelar <lucasbacela@hotmail.com>
2021-02-25 11:58:13 -0300

    Resolução tanto da atividade como do desafio de interpretação

---
+++ semana3/loops/index.js
@@ -0,0 +1,28 @@
+// Enunciado
+// Interpretação
+
+// Exercicio 1
+
+// a. Está definindo uma variável chamado valor e somando todos numeros inteiros entre 1 e 4 a esse valor
+
+// b. O resultado impresso é 10
+
+// Exercicio 2
+
+// a. Todos os numeros da lista maiores que 18, no caso [19, 21, 23, 25, 27, 30]
+
+// b. Se o indice dito for apenas o valor para ser impresso, sim o for...of... pode acessar 
+// os valores do array mas apenas como cópia, não podendo fazer alteração no array.
+
+// Se for para acessar o numero do indice de cada item, o for...of... não é o loop recomendado 
+// pra esse tipo de operação.
+
+// Desafio 1
+
+// a. o resultado seria 0
+//                      00
+//                      000
+// 	                 0000
+
+// Sendo que o for mais interno vai adicionar a quantidade de "0" referente ao valorda variável 
+// "quantidadeAtual" + 1 e dar console log e adicionar +1 a variável até chegar em 4 e parar o loop externo;
