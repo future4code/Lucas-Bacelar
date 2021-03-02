7b5a02449a7776d2b5d34464ba155b6f4414b486
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-02 15:18:59 -0300

    Resolução do exercicio do enunciado

---
+++ semana4/planner/index.js
@@ -0,0 +1,12 @@
+const button = document.getElementById("criar-tarefa")
+const input = document.getElementById("tarefa")
+const weekDay = document.getElementById("dias-semana")
+
+button.onclick = function () {
+    const task = input.value
+    const day = weekDay.value
+
+    const taskDay = document.getElementById(day)
+    taskDay.innerHTML += task
+    input.value = " "
+}
