367abc185922d831880ec2707d6c9bddad396fe4
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-02 16:48:34 -0300

    Começo do quarto desafio

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,20 +1,21 @@
+// Butões para criar e limpar tarefas
 const criar = document.getElementById("criar-tarefa")
 const limpar = document.getElementById("limpar-tarefa")
-const input = document.getElementById("tarefa")
-const weekDay = document.getElementById("dias-semana")
-const itemLista = document.getElementsByClassName("item-lista")
 
+// Input da nova tarefa
+const input = document.getElementById("tarefa")
+
+// Lista de seleção dos dias da semana
+const weekDay = document.getElementById("dias-semana")
+
+// Seção onde estão guardados os dias
+const section = document.querySelector("section")
+
+// Divs contendo as tarefas
 const semanaConteudo = document.querySelectorAll(".semana > div")
 
-function passThrough (e) {
-    if(e.style.textDecoration === "line-through") {
-        e.style.textDecoration = "none"
-    }
-    else {
-        e.style.textDecoration = "line-through"
-    }
-}
 
+// Criar uma tarefa
 criar.onclick = function () {
     if(input.value === "" || input.value === " ") {
         alert("A tarefa está em branco")
@@ -24,16 +25,39 @@ criar.onclick = function () {
         const task = input.value
         const day = weekDay.value
         const taskDay = document.getElementById(day)
-        taskDay.innerHTML += `<p class="item-lista" onclick="passThrough(this)">- ${task}</p>`
+        taskDay.innerHTML += `<p class="item-lista">- ${task}</p>`
         input.value = " "
     }
 }
 
+// Limpar todas as tarefas
 limpar.onclick = function () {
     for(let i = 0, j = semanaConteudo.length; i < j; i++){
         semanaConteudo[i].innerHTML = ""
     }
 }
+
+
+// Riscar a tarefa
+section.onclick = function (e) {
+    if(e.target && e.target.nodeName == "P")
+    {
+        if(e.target.style.textDecoration === "line-through") {
+            e.target.style.textDecoration = "none"
+            e.stopPropagation()
+        }
+        else {
+            e.target.style.textDecoration = "line-through"
+            e.stopPropagation()
+        }
+    }
+}
+
+// Criar horas nas semanas
+for(int i = 1; i <= 24; i++) {
+    
+}
+
 
 
 
