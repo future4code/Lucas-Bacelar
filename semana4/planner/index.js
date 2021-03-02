9340f5b9620189698ecc512f19d0523152ab77a3
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-02 16:03:46 -0300

    Resolução de alguns desafios

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,12 +1,40 @@
-const button = document.getElementById("criar-tarefa")
+const criar = document.getElementById("criar-tarefa")
+const limpar = document.getElementById("limpar-tarefa")
 const input = document.getElementById("tarefa")
 const weekDay = document.getElementById("dias-semana")
+const itemLista = document.getElementsByClassName("item-lista")
 
-button.onclick = function () {
-    const task = input.value
-    const day = weekDay.value
+const semanaConteudo = document.querySelectorAll(".semana > div")
 
-    const taskDay = document.getElementById(day)
-    taskDay.innerHTML += `<p>- ${task}</p>`
-    input.value = " "
+function passThrough (e) {
+    if(e.style.textDecoration === "line-through") {
+        e.style.textDecoration = "none"
+    }
+    else {
+        e.style.textDecoration = "line-through"
+    }
 }
+
+criar.onclick = function () {
+    if(input.value === "" || input.value === " ") {
+        alert("A tarefa está em branco")
+    }
+
+    else {
+        const task = input.value
+        const day = weekDay.value
+        const taskDay = document.getElementById(day)
+        taskDay.innerHTML += `<p class="item-lista" onclick="passThrough(this)">- ${task}</p>`
+        input.value = " "
+    }
+}
+
+limpar.onclick = function () {
+    for(let i = 0, j = semanaConteudo.length; i < j; i++){
+        semanaConteudo[i].innerHTML = ""
+    }
+}
+
+
+
+
