e0b48bfb67dbf3b94e7041670af2879c135ad49d
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-05 15:18:33 -0300

    Exercicio 6

--- semana4/lista-de-exercicios/exercicios.js
+++ semana4/lista-de-exercicios/exercicios.js
@@ -88,3 +88,25 @@ function retornaQuantidadeElementos(array) {
 
   return array.length;
 }
+
+//Exercício 6
+
+function retornaExpressoesBooleanas() {
+  // Solução em que o pc faz o trabalho pra mim kkk
+
+  //   const booleano1 = true;
+  //   const booleano2 = false;
+  //   const booleano3 = !booleano2; true
+  //   const booleano4 = !booleano3; false
+
+  //   let a = booleano1 && booleano2 && !booleano4
+  //   let b = (booleano1 && booleano2) || !booleano3
+  //   let c = (booleano2 || booleano3) && (booleano4 || booleano1)
+  //   let d = !(booleano2 && booleano3) || !(booleano1 && booleano3)
+  //   let e = !(booleano1) && !(booleano3) || (!booleano4 && booleano3 && booleano3)
+
+  //   return [a, b, c, d, e]
+
+  // Retorno das respotas em um array
+  return [false, false, true, true, true];
+}
