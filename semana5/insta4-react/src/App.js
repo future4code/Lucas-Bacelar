0524ab6b15bf88da58bca07bf1ee4873a072af2e
Lucas Bacelar <lucasbacela@hotmail.com>
2021-03-11 15:39:23 -0300

    Resolução do primeiro exercicio do enunciado

--- semana5/insta4-react/src/App.js
+++ semana5/insta4-react/src/App.js
@@ -3,24 +3,41 @@ import './App.css';
 import Post from './components/Post/Post';
 
 class App extends React.Component {
+  state = {
+    postContent: [
+      {
+        nomeUsuarioObj: 'paulinha',
+        fotoUsuarioObj: 'https://picsum.photos/50/50?a=1',
+        fotoPostObj: 'https://picsum.photos/200/150?a=1'
+      },
+      {
+        nomeUsuarioObj: 'Lucas',
+        fotoUsuarioObj: 'https://picsum.photos/50/50?a=2',
+        fotoPostObj: 'https://picsum.photos/200/150?a=2'
+      },
+      {
+        nomeUsuarioObj: 'Ana',
+        fotoUsuarioObj: 'https://picsum.photos/50/50?a=3',
+        fotoPostObj: 'https://picsum.photos/200/150?a=3'
+      }
+    ]
+  }
+
+
   render() {
+
+    const Posts = this.state.postContent.map((post) => {
+      return <Post 
+        nomeUsuario={post.nomeUsuarioObj}
+        fotoUsuario={post.fotoUsuarioObj}
+        fotoPost={post.fotoPostObj} 
+      />
+    })
+    
+
     return (
       <div className={'app-container'}>
-        <Post
-          nomeUsuario={'paulinha'}
-          fotoUsuario={'https://picsum.photos/50/50?a=1'}
-          fotoPost={'https://picsum.photos/200/150?a=1'}
-        />
-        <Post
-          nomeUsuario={'Lucas'}
-          fotoUsuario={'https://picsum.photos/50/50?a=2'}
-          fotoPost={'https://picsum.photos/200/150?a=2'}
-        />
-        <Post
-          nomeUsuario={'Ana'}
-          fotoUsuario={'https://picsum.photos/50/50?a=3'}
-          fotoPost={'https://picsum.photos/200/150?a=3'}
-        />
+        {Posts}
       </div>
     );
   }
